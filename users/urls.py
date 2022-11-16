5bd7aab12322321e959a0baf9871b4f1f9aca882
asuraCoder <asuracoder@gmail.com>
2022-11-16 23:06:11 +0900

    editing

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.Users.as_view()),
+    path("me", views.Me.as_view()),
+    path("password", views.ChangePassword.as_view()),
+    path("login", views.LogIn.as_view()),
+    path("logout", views.LogOut.as_view()),
     path("<int:pk>", views.OneUser.as_view()),
     path("<int:pk>/tweets", views.UserTweets.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.exceptions import NotFound
+from rest_framework.permissions import IsAuthenticated
 from .models import User
+from rest_framework import status
 from tweets.models import Tweet
 from tweets.serializers import TweetSerializer
-from .serializers import TinyUserSerializer
+from .serializers import TinyUserSerializer, PrivateUserSerializer
+from rest_framework.exceptions import ParseError, NotFound
+from django.contrib.auth import authenticate, login, logout
 
 
 class UserTweets(APIView):
@@ -41,3 +44,86 @@ class Users(APIView):
         all_users = User.objects.all()
         serializer = TinyUserSerializer(all_users, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            serializer = PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Me(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        user = request.user
+        serializer = PrivateUserSerializer(
+            user,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            user = serializer.save()
+            serializer = PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class ChangePassword(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        if not old_password or not new_password:
+            raise ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            raise ParseError
+
+
+class LogIn(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user:
+            login(request, user)
+            return Response({"ok": "Welcome!"})
+        else:
+            return Response({"error": "wrong password"})
+
+
+class LogOut(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"ok": "bye!"})
